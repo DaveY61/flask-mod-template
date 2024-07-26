ce599e8405ea2ca0199922fb5a484d0d9aa3e95c
DaveY61 <154756812+DaveY61@users.noreply.github.com>
2024-07-26 10:36:17 -0700

    Moved js to the html file, added style for page

--- app/modules/module_1/module.py
+++ app/modules/module_1/module.py
@@ -8,9 +8,11 @@ def pie_chart():
     values = {'value1': 25, 'value2': 25, 'value3': 25, 'value4': 25}
     
     if request.method == 'POST':
-        values['value1'] = int(request.form.get('value1', 0))
-        values['value2'] = int(request.form.get('value2', 0))
-        values['value3'] = int(request.form.get('value3', 0))
-        values['value4'] = int(request.form.get('value4', 0))
+        values = {
+            'value1': request.form.get('value1', 0),
+            'value2': request.form.get('value2', 0),
+            'value3': request.form.get('value3', 0),
+            'value4': request.form.get('value4', 0)
+        }
     
     return render_template('pages/pie_chart.html', values=values)
