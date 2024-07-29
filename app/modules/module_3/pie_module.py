a1734969a23d5cd934d45060dfd2064b1e59ecde
DaveY61 <154756812+DaveY61@users.noreply.github.com>
2024-07-29 12:52:28 -0700

    Added module_3 (2nd pie chart)

--- app/modules/module_3/pie_module.py
+++ app/modules/module_3/pie_module.py
@@ -6,9 +6,9 @@ blueprint = Blueprint('pie2', __name__,
                       static_url_path='/modules/module_3/static',
                       template_folder='templates')
 
-@blueprint.route('/pie_chart', methods=['GET', 'POST'])
+@blueprint.route('/pie_chart2', methods=['GET', 'POST'])
 @login_required
-def pie_chart():
+def pie_chart2():
     values = {'value1': 25, 'value2': 25, 'value3': 25, 'value4': 25}
     
     if request.method == 'POST':
@@ -19,4 +19,4 @@ def pie_chart():
             'value4': request.form.get('value4', 0)
         }
     
-    return render_template('pages/pie_chart.html', values=values)
+    return render_template('pages/pie_chart2.html', values=values)
