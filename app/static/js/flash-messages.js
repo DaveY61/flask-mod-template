97879528653875dd730398613319ea280799b057
DaveY61 <154756812+DaveY61@users.noreply.github.com>
2024-08-15 16:48:40 -0700

    changed flash message so "danger" (error) messages stick

--- app/static/js/flash-messages.js
+++ app/static/js/flash-messages.js
@@ -1,24 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var flashMessages = document.getElementById('flash-messages');
-  if (flashMessages) {
-      var alerts = flashMessages.getElementsByClassName('alert');
-      for (var i = 0; i < alerts.length; i++) {
-          var alert = alerts[i];
-          if (!alert.classList.contains('no-auto-dismiss')) {
-              setTimeout(function(alertToClose) {
-                  var closeButton = alertToClose.querySelector('.btn-close');
-                  if (closeButton) {
-                      closeButton.click();
-                  }
-              }, 2000, alert); // Auto-dismiss after 2 seconds for messages without 'no-auto-dismiss' class
-          } else {
-              // For 'no-auto-dismiss' messages, we'll add a custom close button
-              var closeButton = alert.querySelector('.btn-close');
-              if (closeButton) {
-                  closeButton.classList.add('btn', 'btn-secondary', 'btn-sm');
-                  closeButton.style.opacity = '1';
-              }
-          }
-      }
-  }
+    var flashMessages = document.getElementById('flash-messages');
+    if (flashMessages) {
+        var alerts = flashMessages.getElementsByClassName('alert');
+        for (var i = 0; i < alerts.length; i++) {
+            var alert = alerts[i];
+            if (alert.classList.contains('alert-danger')) {
+                // Error messages: stay until dismissed
+                var closeButton = alert.querySelector('.btn-close');
+                if (closeButton) {
+                    closeButton.classList.add('btn', 'btn-secondary', 'btn-sm');
+                    closeButton.style.opacity = '1';
+                }
+            } else {
+                // Normal messages: auto-dismiss after 2000 ms
+                setTimeout(function(alertToClose) {
+                    var closeButton = alertToClose.querySelector('.btn-close');
+                    if (closeButton) {
+                        closeButton.click();
+                    }
+                }, 2000, alert);
+            }
+        }
+    }
 });
